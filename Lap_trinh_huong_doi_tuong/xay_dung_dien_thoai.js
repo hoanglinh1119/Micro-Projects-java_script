aa50d7bcfb1b31192aa8f0d5fa0e7ebaa29a037b
hoanglinh1119 <57972639+hoanglinh1119@users.noreply.github.com>
2019-12-12 16:35:59 +0700

    121212

--- Lap_trinh_huong_doi_tuong/xay_dung_dien_thoai.js
+++ Lap_trinh_huong_doi_tuong/xay_dung_dien_thoai.js
@@ -1,3 +1,27 @@
-let Iphone=function () {
-    this.
+let Mobile=function (name,power,battery,draftBox,inputBox,outBox) {
+    this.name=name;
+    this.power=power;
+    this.battery=battery;
+    this.draftBox=draftBox;
+    this.inputBox=inputBox;
+    this.outBox=outBox;
+
+    this.getPower=function () {
+        let powerMobile=document.getElementById("power" + this.name);
+        if(this.power>0){document.getElementById(+this.name+"Turn on"+this.power)} else {
+            document.getElementById(+this.name+"turn off"+this.power)
+        };
+    }
+    this.getBatterry=function (this.power) {
+        while(this.power <100){
+            this.power++;
+            document.getElementById(+this.name+this.power);
+        };
+    }
+    this.getinputBox=function () {
+        let storage=[];
+            if(this.power>0){
+                
+            }
+    }
 }
